7cb86d3e661ee0c2746a6c4be5aaf2f9946c11c3
Dinh Khac Dien <dinhkhacdien03@gmail.com>
2024-07-23 21:02:15 +0700

    Add project files.

---
+++ DinKhacDien/Data/FPTContext.cs
@@ -0,0 +1,19 @@
+﻿using DinKhacDien.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinKhacDien.Data
+{
+    public class FPTContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-045KIG4Q\\SQLEXPRESS;Database=FPT;Trusted_Connection=True;");
+        }
+        public DbSet<Product>? Products { get; set; }
+    }
+}
---
+++ DinKhacDien/Migrations/20240723135516_InitialData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DinKhacDien.Migrations
+{
+    public partial class InitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
